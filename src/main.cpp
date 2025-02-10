7271abc3a876d5841699f8f9425168cef8961518
Bruce Nguyen <bruceleenguyen95@gmail.com>
2025-02-10 13:15:35 -0700

    feat(lox): added argument checking and functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
+#include <fstream>
 #include <iostream>
 
-int main() {
-  std::cout << "Hello, World!\n";
+void run_file(std::string path) {
+  std::ifstream inf { path };
+  if (!inf) {
+    std::cerr << "Invalid script\n";
+    return;
+  }
+
+  std::string input {};
+  while (std::getline(inf, input)) {
+    std::cout << input << '\n';
+  }
+}
+
+void run(std::string source) {}
+
+void run_prompt() {
+  while (true) {
+    std::cout << "> ";
+    std::string input {};
+    std::cin >> input;
+    run(input);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  if (argc > 2) {
+    std::cerr << "Usage: lox [script]\n";
+    return 1;
+  } else if (argc == 2) {
+    run_file(argv[1]);
+  } else {
+    run_prompt();
+  }
   return 0;
 }
