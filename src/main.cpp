bbe0e1a4b37b4c862266a90199f483eff295f27f
Bruce Nguyen <bruceleenguyen95@gmail.com>
2025-02-10 13:40:21 -0700

    feat: added error methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,41 @@
 #include <fstream>
 #include <iostream>
+#include <iterator>
 
-void run_file(std::string path) {
+static bool had_error { false };
+
+static void report(int line, std::string_view where, std::string_view message) {
+  std::cerr << "[line " << line << "] Error" << where << ": " << message;
+  had_error = true;
+}
+
+static void error(int line, std::string_view message) {
+  report(line, "", message);
+}
+
+static void run(std::string source) {}
+
+static void run_file(std::string path) {
   std::ifstream inf { path };
   if (!inf) {
     std::cerr << "Invalid script\n";
     return;
   }
 
-  std::string input {};
-  while (std::getline(inf, input)) {
-    std::cout << input << '\n';
-  }
+  std::string source {
+    std::istreambuf_iterator<char>(inf),
+    std::istreambuf_iterator<char>(),
+  };
+
+  run(std::move(source));
 }
 
-void run(std::string source) {}
-
-void run_prompt() {
+static void run_prompt() {
   while (true) {
     std::cout << "> ";
-    std::string input {};
-    std::cin >> input;
-    run(input);
+    std::string line {};
+    std::cin >> line;
+    run(std::move(line));
   }
 }
 
