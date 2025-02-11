6f13eca83193e75af3c97f3d9451842e816f6726
Bruce Nguyen <bruceleenguyen95@gmail.com>
2025-02-10 19:17:56 -0700

    feat: updated exit code for usage

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,7 @@ int main(int argc, char* argv[])
 {
   if (argc > 2) {
     std::cerr << "Usage: lox [script]\n";
-    return 1;
+    return 64;
   } else if (argc == 2) {
     run_file(argv[1]);
   } else {
